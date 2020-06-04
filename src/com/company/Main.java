9cbcac541b9a366c01fda4bcfa0c50be7a8a3194
PAKILA0 <wowo789aa@gmail.com>
2020-06-04 19:25:19 +0800

    Add date converter and Q1(2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) throws Exception  {
 	    // DATA INPUT
@@ -50,10 +54,21 @@ public class Main {
         //Q1
         System.out.println("Q1(1) : ");
         //List result = students.stream().sorted(comparing(Student::getId)).collect(toList());
-
-        Stream<Student> Q1Output = students.stream().sorted(comparing(Student::getId));
-        Q1Output.forEach(student -> System.out.println(student.Info.FirstName + " " + student.Info.LastName));
-
+        Stream<Student> Q1OutputA = students.stream().sorted(comparing(Student::getId));
+        Q1OutputA.forEach(student -> System.out.println(student.Info.FirstName + " " + student.Info.LastName));
+        System.out.println("Q1(2) : ");
+        Stream<Student> Q1OutputB = students.stream().sorted((o1, o2) -> {
+            try{
+                return o2.getDate().compareTo(o1.getDate());
+            }catch (ParseException e){
+                System.out.println("Date Converter Error");
+            }
+            return 0;
+        });
+        Q1OutputB.forEach(student -> System.out.println(student.Info.FirstName + " " + student.Info.LastName));
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Q2
+        System.out.println("Q2 : ");
 
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,12 +1,19 @@
 package com.company;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Student {
     String Id;
     Person Info = new Person();
     ScoreSheet Scores = new ScoreSheet();
 
-
     public int getId(){
         return Integer.parseInt(this.Id);
+    }
+
+    public Date getDate() throws ParseException {
+        Date result = new SimpleDateFormat("dd/MM/yyyy").parse(this.Info.BirthDay);
+        return result;
     }
 }
